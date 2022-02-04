9c7aa27e775559ce5787c4d49578642981d676aa
Yufit Code <yusranabdulah@gmail.com>
2022-02-04 23:15:15 +0800

    Membuat AuthController

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+//Protecting Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    // API route for logout user
     Route::post('/logout', [AuthController::class, 'logout']);
 });
